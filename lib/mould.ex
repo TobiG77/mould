c3025eb64fca2c1ca4f542f88a72d4e5db3742bb
Tobias Gerschner <tobias.gerschner@gmail.com>
2017-05-09 16:27:53 +1200

    initial commit

---
+++ lib/mould.ex
@@ -0,0 +1,18 @@
+defmodule Mould do
+  @moduledoc """
+  Documentation for Mould.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mould.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
